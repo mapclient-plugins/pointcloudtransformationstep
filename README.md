b4e4cd334a2a63cd10eb505d6f91edf1544df3ab
juzhang <jzha263@aucklanduni.ac.nz>
2015-01-20 18:25:01 +1300

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+pointcloudtransformationstep
+==============================
+
+Step for applying an input geometric transformation to a point cloud
+
+Requires:
+GIAS: https://bitbucket.org/jangle/gias
+mappluginutils: https://bitbucket.org/jangle/mappluginutils
---
+++ mapclientplugins/pointcloudtransformationstep/step.py
@@ -0,0 +1,177 @@
+'''
+MAP Client, a program to generate detailed musculoskeletal models for OpenSim.
+    Copyright (C) 2012  University of Auckland
+    
+This file is part of MAP Client. (http://launchpad.net/mapclient)
+
+    MAP Client is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    MAP Client is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MAP Client.  If not, see <http://www.gnu.org/licenses/>..
+'''
+import os
+
+from PySide import QtGui
+from PySide import QtCore
+
+from mapclient.mountpoints.workflowstep import WorkflowStepMountPoint
+from mapclientplugins.pointcloudtransformationstep.configuredialog import ConfigureDialog
+
+from gias.common import transform3D
+
+_transformFunctions = {
+                'affine': transform3D.transformAffine,
+                'rigid': transform3D.transformRigid3D,
+                'rigidscale': transform3D.transformRigidScale3D,
+                'rigid_about_point': transform3D.transformRigid3DAboutP,
+                'rigidscale_about_point': transform3D.transformRigidScale3DAboutP,
+                }
+
+class PointCloudTransformationStep(WorkflowStepMountPoint):
+    '''
+    Skeleton step which is intended to be a helpful starting point
+    for new steps.
+    '''
+
+    def __init__(self, location):
+        super(PointCloudTransformationStep, self).__init__('Point Cloud Transformation', location)
+        self._configured = False # A step cannot be executed until it has been configured.
+        self._category = 'Registration'
+        # Add any other initialisation code here:
+        # Ports:
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#pointcloud'))
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
+                      'ju#geometrictransform'))
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#pointcloud'))
+        self._config = {}
+        self._config['identifier'] = ''
+
+        self._inputPoints = None
+        self._transform = None
+        self._outputPoints = None
+
+
+    def execute(self):
+        '''
+        Add your code here that will kick off the execution of the step.
+        Make sure you call the _doneExecution() method when finished.  This method
+        may be connected up to a button in a widget for example.
+        '''
+        # Put your execute step code here before calling the '_doneExecution' method.
+
+        try:
+            transformFunction = _transformFunctions[self._transform.transformType]
+        except KeyError:
+            raise RuntimeError, 'unknown transform type: '+self._transform.transformType
+
+        if 'about_point' not in self._transform.transformType:
+            self._outputPoints = transformFunction(
+                                    self._inputPoints,
+                                    self._transform.getT(),
+                                    )
+        else:
+            self._outputPoints = transformFunction(
+                                    self._inputPoints,
+                                    self._transform.getT(),
+                                    self._transform.getP(),
+                                    )
+        self._doneExecution()
+
+    def setPortData(self, index, dataIn):
+        '''
+        Add your code here that will set the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        uses port for this step then the index can be ignored.
+        '''
+        if index == 0:
+            self._inputPoints = dataIn # ju#pointcoordinates
+        else:
+            self._transform = dataIn # ju#geometrictransform
+
+    def getPortData(self, index):
+        '''
+        Add your code here that will return the appropriate objects for this step.
+        The index is the index of the port in the port list.  If there is only one
+        provides port for this step then the index can be ignored.
+        '''
+        return self._outputPoints # ju#pointcoordinates
+
+    def configure(self):
+        '''
+        This function will be called when the configure icon on the step is
+        clicked.  It is appropriate to display a configuration dialog at this
+        time.  If the conditions for the configuration of this step are complete
+        then set:
+            self._configured = True
+        '''
+        dlg = ConfigureDialog()
+        dlg.identifierOccursCount = self._identifierOccursCount
+        dlg.setConfig(self._config)
+        dlg.validate()
+        dlg.setModal(True)
+        
+        if dlg.exec_():
+            self._config = dlg.getConfig()
+        
+        self._configured = dlg.validate()
+        self._configuredObserver()
+
+    def getIdentifier(self):
+        '''
+        The identifier is a string that must be unique within a workflow.
+        '''
+        return self._config['identifier']
+
+    def setIdentifier(self, identifier):
+        '''
+        The framework will set the identifier for this step when it is loaded.
+        '''
+        self._config['identifier'] = identifier
+
+    def serialize(self, location):
+        '''
+        Add code to serialize this step to disk.  The filename should
+        use the step identifier (received from getIdentifier()) to keep it
+        unique within the workflow.  The suggested name for the file on
+        disk is:
+            filename = getIdentifier() + '.conf'
+        '''
+        configuration_file = os.path.join(location, self.getIdentifier() + '.conf')
+        conf = QtCore.QSettings(configuration_file, QtCore.QSettings.IniFormat)
+        conf.beginGroup('config')
+        conf.setValue('identifier', self._config['identifier'])
+        conf.endGroup()
+
+
+    def deserialize(self, location):
+        '''
+        Add code to deserialize this step from disk.  As with the serialize 
+        method the filename should use the step identifier.  Obviously the 
+        filename used here should be the same as the one used by the
+        serialize method.
+        '''
+        configuration_file = os.path.join(location, self.getIdentifier() + '.conf')
+        conf = QtCore.QSettings(configuration_file, QtCore.QSettings.IniFormat)
+        conf.beginGroup('config')
+        self._config['identifier'] = conf.value('identifier', '')
+        conf.endGroup()
+
+        d = ConfigureDialog()
+        d.identifierOccursCount = self._identifierOccursCount
+        d.setConfig(self._config)
+        self._configured = d.validate()
+
+
